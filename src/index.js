44a2c8257e0e60142943246df201b62dbb1f32b2
ibaslogic <ibaskunle@gmail.com>
2021-01-06 04:25:55 +0100

    testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 //component
 import TodoContainer from "./functionBased/components/TodoContainer"
@@ -11,7 +11,7 @@ import "./functionBased/App.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>  
   </React.StrictMode>, 
