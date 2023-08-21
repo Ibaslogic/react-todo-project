0485c75680fed0b47a38ce3f048c7db9cf17666a
dvcorreia1984 <victor@onsleer.co.za>
2023-08-21 11:29:15 +0200

    Added dynamic styling to TodoItem.jsx file

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,14 @@
 import styles from '@/styles/TodoItem.module.css'
 
 const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  };
+
   return (
   <li className={styles.item}>
     <div className={styles.content}>
@@ -9,7 +17,9 @@ const TodoItem = ({ itemProp, handleChange, delTodo }) => {
       onChange={() => handleChange(itemProp.id)}
       />
       <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+      <span style={itemProp.completed ? completedStyle : null}>
       {itemProp.title}
+      </span>
     </div>
   </li>
   );
